1e1cbc4e5cb995a8b368150309e0cdf8b3e5c737
Erez <erez.yosepov@trucknet.io>
2021-07-11 14:02:28 +0300

    build a debug 3d UI

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Three.js Journey
+# Three.js Debug UI
 
 ## Setup
 Download [Node.js](https://nodejs.org/en/download/).
--- src/script.js
+++ src/script.js
@@ -2,7 +2,27 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'dat.gui';
 
+// Debug Dat.GUI 
+const gui = new dat.GUI({closed: true, width: 400});
+
+function spin () {
+gsap.to(mesh.rotation, {duration:1, y: mesh.rotation.y +10});
+};
+
+
+const parameters = {
+    color: 0xff0000,
+    spin
+};
+
+gui.addColor(parameters, 'color')
+.onChange(() =>{
+    material.color.set( parameters.color);
+});
+
+gui.add(parameters, 'spin');
 /**
  * Base
  */
@@ -16,9 +36,23 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
 const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+scene.add(mesh);
+
+//Debug
+gui.add(mesh.position, 'y')
+.min(-3)
+.max(3)
+.step(0.01)
+.name('elevation');
+// gui.add(mesh.position, 'y', -3, 3, 0.01);
+// gui.add(mesh.position, 'x', -3, 3, 0.01);
+// gui.add(mesh.position, 'z', -3, 3, 0.01);
+
+gui.add(mesh, 'visible');
+gui.add(mesh.material, 'wireframe');
+
 
 /**
  * Sizes
